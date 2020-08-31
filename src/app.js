230305bbc8e84912e84359907e5af4fd6c71cb3d
Jeff Chew <jeff.chew@gmail.com>
2020-08-31 17:40:48 -0400

    chore(testapp): sample webpack/handlebars application
    This is a basic webpack 4 / handlebars application that utilizes the web
     components variant of the IBM.com Library. The idea is that this a test
      to see how adopters might be integrating the web-components project,
      and attempt to identify any issues that they may run into.
    
    Currently, the `dotcom-shell-container` is activated (with
    `masthead-container` and `footer-container` commented out).

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/dotcom-shell-container';
+import './assets/scss/app.scss';
+// import '@carbon/ibmdotcom-web-components/es/components/masthead/masthead-container';
+// import '@carbon/ibmdotcom-web-components/es/components/footer/footer-container';
